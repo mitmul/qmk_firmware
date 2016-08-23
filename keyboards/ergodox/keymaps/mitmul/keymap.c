ae2928d26d8cd241c6da1dffa6c83a8d36e8f240
Shunta Saito <shuntasaito@fb.com>
2016-08-23 16:38:58 +0800

    I have crab allergy

--- keyboards/ergodox/keymaps/mitmul/keymap.c
+++ keyboards/ergodox/keymaps/mitmul/keymap.c
@@ -11,21 +11,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 0: Basic layer
  *
  * ,--------------------------------------------------.           ,-----------------------------------------------------.
- * |   =    |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   -       |
+ * |  ESC   |  `   |   1  |   2  |   3  |   4  |   5  |           |   6  |   7  |   8  |   9  |   0  |   -  |    =      |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+-----------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  | Backspace |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |  Bs  |           |  Bs  |   Y  |   U  |   I  |   O  |   P  |     \     |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+-----------|
- * | CTL_T  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2| Enter     |
+ * | Ctrl   |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |     '     |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+-----------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  | LANG1|           | LANG2|   N  |   M  |   ,  |   .  |//Ctrl| RShift    |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  | LANG1|           | LANG2|   N  |   M  |   ,  |   .  |//Ctrl|  Enter    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+-----------'
- *   |~L1   |  [   |   ]  | Alt  | LGui |                                       | Left | Right| Up   | Down |  ' / Cmd  |
+ *   |~L1   |  [   |   ]  | Alt  | LGui |                                       | Left | Right| Up   | Right| ' / Cmd   |
  *   `----------------------------------'                                       `---------------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  | Esc  |
+ *                                        |  L1  |  L2  |       |  [   |   ]  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | Space|Backsp|------|       |------|  Tab   |Space |
+ *                                 | Space|Backsp|------|       |------|  Tab   |Enter |
  *                                 |      |ace   | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -33,23 +33,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_EQL,         KC_1,         KC_2,     KC_3,       KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,         KC_Q,         KC_W,     KC_E,       KC_R,   KC_T,   TG(SYMB),
-        KC_LCTL,        KC_A,         KC_S,     KC_D,       KC_F,   KC_G,
-        KC_LSFT,        CTL_T(KC_Z),  KC_X,     KC_C,       KC_V,   KC_B,   ALL_T(KC_LANG1),
-        KC_FN1,         KC_LBRC,      KC_RBRC,  KC_LALT,    KC_LGUI,
-                                              ALT_T(KC_APP),    KC_LGUI,
-                                                                KC_HOME,
-                                               KC_SPC,KC_BSPC,  KC_END,
+        KC_ESC,         KC_GRV,       KC_1,         KC_2,           KC_3,       KC_4,   KC_5,
+        KC_TAB,         KC_Q,         KC_W,         KC_E,           KC_R,       KC_T,   KC_BSPC,
+        KC_LCTL,        KC_A,         KC_S,         KC_D,           KC_F,       KC_G,
+        KC_LSFT,        KC_Z,         KC_X,         KC_C,           KC_V,       KC_B,   ALL_T(KC_LANG1),
+        KC_FN1,         KC_LBRC,      KC_RBRC,      KC_LALT,        KC_LGUI,
+                                                    TG(SYMB),       TG(MDIA),
+                                                                    KC_HOME,
+                                            KC_SPC, KC_BSPC,        KC_END,
         // right hand
-             KC_RGHT,           KC_6,   KC_7,       KC_8,       KC_9,       KC_0,               KC_MINS,
-             TG(SYMB),          KC_Y,   KC_U,       KC_I,       KC_O,       KC_P,               KC_BSPC,
-                                KC_H,   KC_J,       KC_K,       KC_L,       LT(MDIA, KC_SCLN),  KC_ENT,
-             MEH_T(KC_LANG2),   KC_N,   KC_M,       KC_COMM,    KC_DOT,     CTL_T(KC_SLSH),     KC_RSFT,
-                                        KC_LEFT,    KC_RGHT,    KC_UP  ,    KC_DOWN,            GUI_T(KC_QUOT),
-             KC_LALT,       KC_ESC,
+             KC_6,              KC_7,   KC_8,       KC_9,       KC_0,       KC_MINS,            KC_EQL,
+             KC_BSPC,           KC_Y,   KC_U,       KC_I,       KC_O,       KC_P,               KC_BSLS,
+                                KC_H,   KC_J,       KC_K,       KC_L,       KC_SCLN,            KC_QUOT,
+             MEH_T(KC_LANG2),   KC_N,   KC_M,       KC_COMM,    KC_DOT,     CTL_T(KC_SLSH),     KC_ENT,
+                                        KC_LEFT,    KC_RGHT,    KC_UP,      KC_DOWN,            GUI_T(KC_QUOT),
+             KC_LBRC,       KC_RBRC,
              KC_PGUP,
-             KC_PGDN,       KC_TAB, KC_SPC
+             KC_PGDN,       KC_TAB, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
